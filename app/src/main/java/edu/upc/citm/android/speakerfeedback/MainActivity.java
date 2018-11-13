f6e007b44013a4ea8461227c6499522806b05ed4
albertln <d.pocafeiner@gmail.com>
2018-11-13 13:37:23 +0100

    afegim 2 listeners per saber quan es modifica la base de dades

--- app/src/main/java/edu/upc/citm/android/speakerfeedback/MainActivity.java
+++ app/src/main/java/edu/upc/citm/android/speakerfeedback/MainActivity.java
@@ -12,7 +12,12 @@ import android.widget.Toast;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.ListenerRegistration;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -23,6 +28,8 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private TextView textview;
     private String userId;
+    private ListenerRegistration roomRegistration;
+    private ListenerRegistration usersRegistration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +38,63 @@ public class MainActivity extends AppCompatActivity {
 
         textview = findViewById(R.id.textview);
 
+        getOnRegisterUser();
+    }
+
+    private EventListener<DocumentSnapshot> roomListener = new EventListener<DocumentSnapshot>() {
+        @Override
+        public void onEvent(DocumentSnapshot documentSnapshot, FirebaseFirestoreException e) {
+            if (e != null)
+            {
+                Log.e("SpeakerFeedback", "Error al rebre rooms/testroom", e);
+                return;
+            }
+
+            String name = documentSnapshot.getString("name");
+            setTitle(name);
+        }
+    };
+
+    private EventListener<QuerySnapshot> usersListener = new EventListener<QuerySnapshot>() {
+        @Override
+        public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+            if (e != null)
+            {
+                Log.e("SpeakerFeedback", "Error al rebre usuaris dins d'un room", e);
+                return;
+            }
+
+            //textview.setText(String.format("Numuser: %d", documentSnapshots.size()));
+
+            String nomsUsuaris = "";
+            for (DocumentSnapshot doc : documentSnapshots)
+            {
+                nomsUsuaris += doc.getString("name") + "\n";
+            }
+
+            textview.setText(nomsUsuaris);
+        }
+    };
+
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        roomRegistration = db.collection("rooms").document("testroom").addSnapshotListener(roomListener);
+
+        usersRegistration = db.collection("users").whereEqualTo("room", "testroom").addSnapshotListener(usersListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        roomRegistration.remove();
+        usersRegistration.remove();
+    }
+
+    private void getOnRegisterUser() {
         // Busquem a les preferències de l'app l'ID de l'usuari per saber si ja s'havia registrat
         SharedPreferences prefs = getSharedPreferences("config", MODE_PRIVATE);
         userId = prefs.getString("userId", null);
